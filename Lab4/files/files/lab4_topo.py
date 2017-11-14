7d70b909e29e3a11635c4752a000d628d129ee40
Marin THIERCELIN <marin.thiercelin@epfl.ch>
2017-11-14 11:47:09 +0100

    start

--- Lab4/files/files/lab4_topo.py
+++ Lab4/files/files/lab4_topo.py
@@ -9,7 +9,7 @@ import os
 # You can show useful information during script execution
 from mininet.log import info
 
-# Class in mininext which includes PID namespaces, log and run isolation. 
+# Class in mininext which includes PID namespaces, log and run isolation.
 from mininext.topo import Topo
 
 # Class in mininext to setup the quagga service on router nodes
@@ -31,7 +31,7 @@ class Lab4Topo(Topo):
 		"""Initialize a Quagga topology with 2 hosts, 5 routers, 7 switches, quagga service, in a topology according to
 	Fig 1. of Lab4"""
 		Topo.__init__(self)
-		
+
 		info( '*** Creating Quagga Routers\n' )
 		# Absolute path to this python script
 		selfPath = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -39,7 +39,7 @@ class Lab4Topo(Topo):
 		# Initialize the Quagga Service
 		# autoStart=True (default) --> starts automatically quagga on the host
 		# autoStop=True (default) --> stops automatically quagga (we don't want this)
-		quaggaSvc = QuaggaService(autoStart=False, autoStop=False) 
+		quaggaSvc = QuaggaService(autoStart=True, autoStop=False) 
 
 		# Configuration file path for quagga routers
 		# We require a "config" folder in the same path of the lab4_topo file, and within
@@ -50,7 +50,7 @@ class Lab4Topo(Topo):
 		# Initializing local variables
 		netHosts = []
 		NodeList = []
-		
+
 		# List of all hosts in the network.
 		# Note that each node requires at least one IP address to avoid
 		# Mininet's automatic IP address assignment
@@ -61,11 +61,11 @@ class Lab4Topo(Topo):
 		netHosts.append(NetworkHosts(name='r3', IP='10.10.23.3/24', DG=''))
 		netHosts.append(NetworkHosts(name='r4', IP='10.10.14.4/24', DG=''))
 		netHosts.append(NetworkHosts(name='r5', IP='10.10.25.5/24', DG=''))
-	
+
 		for host in netHosts:
 			# We create a list of node names
 			NodeList.append(host.name)
-			if host.name in ['h1','h2']: 
+			if host.name in ['h1','h2']:
 				# We configure PCs with default gateway and without quagga service
 				AddPCHost = self.addHost(name=host.name, ip=host.IP, defaultRoute=host.DG, hostname=host.name,  privateLogDir=True, privateRunDir=True, inMountNamespace=True, inPIDNamespace=True, inUTSNamespace=True)
 			else :
@@ -75,7 +75,7 @@ class Lab4Topo(Topo):
 				# Note that we require one folder for each host, named as the host itself
 				quaggaSvcConfig = {'quaggaConfigPath': quaggaBaseConfigPath + host.name}
 				self.addNodeService(node=host.name, service=quaggaSvc, nodeConfig=quaggaSvcConfig)
-				
+
 		# Adding switches to the network, we specify OpenFlow v1.3 for better IPv6 multicast support
 		SW1 = self.addSwitch('SW1', protocols='OpenFlow13')
 		SW2 = self.addSwitch('SW2', protocols='OpenFlow13')
@@ -89,25 +89,24 @@ class Lab4Topo(Topo):
 		info( '*** Creating links\n' )
 		self.addLink( SW1, NodeList[0], intfName2='h1-eth1'  )
 		self.addLink( SW1, NodeList[2], intfName2='r1-eth3'  )
-		
+
 		self.addLink( SW2, NodeList[2], intfName2='r1-eth1'  )
 		self.addLink( SW2, NodeList[3], intfName2='r2-eth1'  )
-		
+
 		self.addLink( SW3, NodeList[3], intfName2='r2-eth2'  )
 		self.addLink( SW3, NodeList[4], intfName2='r3-eth1'  )
-		
+
 		self.addLink( SW4, NodeList[2], intfName2='r1-eth2'  )
 		self.addLink( SW4, NodeList[5], intfName2='r4-eth1'  )
-		
+
 		self.addLink( SW5, NodeList[3], intfName2='r2-eth3'  )
 		self.addLink( SW5, NodeList[6], intfName2='r5-eth1'  )
-		
+
 		self.addLink( SW6, NodeList[5], intfName2='r4-eth2'  )
 		self.addLink( SW6, NodeList[6], intfName2='r5-eth3'  )
 
 		self.addLink( SW7, NodeList[4], intfName2='r3-eth2'  )
 		self.addLink( SW7, NodeList[6], intfName2='r5-eth2'  )
-		
+
 		self.addLink( SW8, NodeList[3], intfName2='r2-eth4'  )
 		self.addLink( SW8, NodeList[1], intfName2='h2-eth1'  )
-		
